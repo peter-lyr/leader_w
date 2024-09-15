0d642e3a20098d4b32a95687899dca5c0ec942db
peter-lyr <llydrp.ldp@gmail.com>
2024-09-15 16:43:59 +0800

    F.window_delete()

--- lua/leader_w.lua
+++ lua/leader_w.lua
@@ -1,11 +1,4 @@
-local M = {}
 local F = require 'f'
-
-function M.window_delete()
-  if not F.is(F.is_cur_last_win()) then
-    vim.cmd 'q'
-  end
-end
 
 require 'which-key'.add {
   { '<leader>w',   group = 'window', },
@@ -13,7 +6,9 @@ require 'which-key'.add {
   { '<leader>wk',  function() vim.cmd 'wincmd k' end, desc = 'window go up',       mode = { 'n', 'v', }, },
   { '<leader>wh',  function() vim.cmd 'wincmd h' end, desc = 'window go left',     mode = { 'n', 'v', }, },
   { '<leader>wl',  function() vim.cmd 'wincmd l' end, desc = 'window go right',    mode = { 'n', 'v', }, },
-  { '<leader>wd',  function() M.window_delete() end,  desc = 'window delete',      mode = { 'n', 'v', }, },
+
+  { '<leader>wd',  function() F.window_delete() end,  desc = 'window delete cur',  mode = { 'n', 'v', }, },
+
   { '<leader>ws',  group = 'window.split', },
   { '<leader>wsj', '<c-w>s',                          desc = 'window.split down',  mode = { 'n', 'v', }, },
   { '<leader>wsk', '<c-w>s<c-w>k',                    desc = 'window.split up',    mode = { 'n', 'v', }, },
