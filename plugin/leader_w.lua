a0316fff3aa0961024efa82105852fdbc33bcf83
peter-lyr <llydrp.ldp@gmail.com>
2024-10-01 08:33:53 +0800

    <leader>woj new_win_finc_down

--- plugin/leader_w.lua
+++ plugin/leader_w.lua
@@ -29,7 +29,19 @@ require 'which-key'.register {
   ['<leader>wih'] = { function() F.new_win_ftail_left() end, 'window.new.tail left', mode = { 'n', 'v', }, },
   ['<leader>wil'] = { function() F.new_win_ftail_right() end, 'window.new.tail right', mode = { 'n', 'v', }, },
 
+  ['<leader>wo'] = { name = 'window.new.inc', },
+  ['<leader>woj'] = { function() F.new_win_finc_down() end, 'window.new.inc down', mode = { 'n', 'v', }, },
+  ['<leader>wok'] = { function() F.new_win_finc_up() end, 'window.new.inc up', mode = { 'n', 'v', }, },
+  ['<leader>woh'] = { function() F.new_win_finc_left() end, 'window.new.inc left', mode = { 'n', 'v', }, },
+  ['<leader>wol'] = { function() F.new_win_finc_right() end, 'window.new.inc right', mode = { 'n', 'v', }, },
+
   ['<leader>we'] = { '<c-w>=', 'window equal', mode = { 'n', 'v', }, },
   ['<leader>wm'] = { function() F.win_max_height() end, 'window max height', mode = { 'n', 'v', }, },
   ['<leader>w,'] = { function() F.win_max_width() end, 'window max width', mode = { 'n', 'v', }, },
+
+  ['<leader>wJ'] = { function() vim.cmd 'wincmd J' end, 'window be most down', mode = { 'n', 'v', }, },
+  ['<leader>wK'] = { function() vim.cmd 'wincmd K' end, 'window be most up', mode = { 'n', 'v', }, },
+  ['<leader>wH'] = { function() vim.cmd 'wincmd H' end, 'window be most left', mode = { 'n', 'v', }, },
+  ['<leader>wL'] = { function() vim.cmd 'wincmd L' end, 'window be most right', mode = { 'n', 'v', }, },
+  ['<leader>wT'] = { function() vim.cmd 'wincmd T' end, 'window be in new tab', mode = { 'n', 'v', }, },
 }
