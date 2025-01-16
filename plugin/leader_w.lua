1efcee4a8cacfb6f8da57fc6d2cdc5ef4156ab6a
peter-lyr <llydrp.ldp@gmail.com>
2025-01-16 22:11:58 +0800

    <leader>wst <c-w>v<c-w>T

--- plugin/leader_w.lua
+++ plugin/leader_w.lua
@@ -17,12 +17,14 @@ require 'which-key'.register {
   ['<leader>wsk'] = { '<c-w>s<c-w>k', 'window.split up', mode = { 'n', 'v', }, },
   ['<leader>wsh'] = { '<c-w>v<c-w>h', 'window.split left', mode = { 'n', 'v', }, },
   ['<leader>wsl'] = { '<c-w>v', 'window.split right', mode = { 'n', 'v', }, },
+  ['<leader>wst'] = { '<c-w>v<c-w>T', 'window.split tab', mode = { 'n', 'v', }, },
 
   ['<leader>wn'] = { name = 'window.new', },
   ['<leader>wnj'] = { ':<c-u>new<cr>', 'window.new down', mode = { 'n', 'v', }, },
   ['<leader>wnk'] = { ':<c-u>leftabove new<cr>', 'window.new up', mode = { 'n', 'v', }, },
   ['<leader>wnh'] = { ':<c-u>leftabove vnew<cr>', 'window.new left', mode = { 'n', 'v', }, },
   ['<leader>wnl'] = { ':<c-u>vnew<cr>', 'window.new right', mode = { 'n', 'v', }, },
+  ['<leader>wnt'] = { ':<c-u>tabnew<cr>', 'window.new tab', mode = { 'n', 'v', }, },
 
   ['<leader>wi'] = { name = 'window.new.tail', },
   ['<leader>wij'] = { function() F.new_win_ftail_down() end, 'window.new.tail down', mode = { 'n', 'v', }, },
